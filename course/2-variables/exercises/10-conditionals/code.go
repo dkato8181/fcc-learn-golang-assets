254d552967ae26bdd373e931c01ec03d4bd71a6f
davidk <dkato81@gmail.com>
2024-05-08 22:58:49 +0300

    complete conditionals

--- course/2-variables/exercises/10-conditionals/code.go
+++ course/2-variables/exercises/10-conditionals/code.go
@@ -7,9 +7,7 @@ func main() {
 	maxMessageLen := 20
 	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
 
-	// don't touch above this line
-
-	if messageLen > maxMessageLen {
+	if messageLen <= maxMessageLen {
 		fmt.Println("Message sent")
 	} else {
 		fmt.Println("Message not sent")
