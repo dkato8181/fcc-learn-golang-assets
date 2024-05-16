5587f039229daca4afd4329cf3bdf8785d1225bc
davidk <dkato81@gmail.com>
2024-05-16 12:59:59 +0300

    complete errors package

--- course/6-errors/exercises/5-errors_package/code.go
+++ course/6-errors/exercises/5-errors_package/code.go
@@ -7,12 +7,10 @@ import (
 
 func divide(x, y float64) (float64, error) {
 	if y == 0 {
-		// ?
+		return 0.0, errors.New("no dividing by 0")
 	}
 	return x / y, nil
 }
-
-// don't edit below this line
 
 func test(x, y float64) {
 	defer fmt.Println("====================================")
