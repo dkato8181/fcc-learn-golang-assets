ff78a445a4a6262dc959e5b0b960197eca856151
davidk <dkato81@gmail.com>
2024-05-16 12:59:28 +0300

    complete custom errors

--- course/6-errors/exercises/3-custom_errors/code.go
+++ course/6-errors/exercises/3-custom_errors/code.go
@@ -8,9 +8,9 @@ type divideError struct {
 	dividend float64
 }
 
-// ?
-
-// don't edit below this line
+func (d divideError) Error() string {
+	return fmt.Sprintf("can not divide %v by zero", d.dividend)
+}
 
 func divide(dividend, divisor float64) (float64, error) {
 	if divisor == 0 {
